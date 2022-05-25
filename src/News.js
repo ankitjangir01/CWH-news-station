b1f23a24457fc99be550781b936a20e73972672d
ankitjangir01 <93923854+ankitjangir01@users.noreply.github.com>
2022-05-25 21:13:10 +0530

    adding prev and next page buttons
    both the buttons are working by changing the api url and fetching the data again for new page. Also counting the page numbers from Math.ceil(totalResults/resultsPerPage). We tool resultsPerPage as 12 here, do not forget to mention results per page in the api url.

--- src/News.js
+++ src/News.js
@@ -9,16 +9,39 @@ export default class News extends Component {
         super();
         this.state = {
             articles: this.articles,
-            loading: false
+            loading: false,
+            page: 1
         }
     }
 
-    async componentDidMount(){
-        let url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=48ccfd98f01b4ff995fe26caf9cbd7fb';
+    async componentDidMount() {
+        let url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=48ccfd98f01b4ff995fe26caf9cbd7fb&pageSize=12';
         let data = await fetch(url);
-        let parsedData =  await data.json();
-        this.setState({articles: parsedData.articles})
+        let parsedData = await data.json();
+        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults })
 
+    }
+
+    handleNext = async () => {
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 12)) { }
+        else{
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=48ccfd98f01b4ff995fe26caf9cbd7fb&page=${this.state.page + 1}&pageSize=12`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            this.setState({
+                articles: parsedData.articles,
+                page: this.state.page + 1,
+            })
+        }
+    }
+    handlePrev = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=48ccfd98f01b4ff995fe26caf9cbd7fb&page=${this.state.page - 1}&pageSize=12`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+            articles: parsedData.articles,
+            page: this.state.page - 1,
+        })
     }
 
     render() {
@@ -29,12 +52,15 @@ export default class News extends Component {
                     {this.state.articles.map((element) => {
                         return (
                             <div className='col-md-4' key={element.url}>
-                                <NewsItem title={element.title} description={element.description? element.description.slice(0,100) : ""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                                <NewsItem title={element.title} description={element.description ? element.description.slice(0, 100) : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
                             </div>
                         )
                     })}
                 </div>
-
+                <div className='d-flex justify-content-around my-5'>
+                    <button type="button" disabled={this.state.page <= 1} className="btn btn-secondary" onClick={this.handlePrev}>&larr; Previous Page</button>
+                    <button type="button" className="btn btn-secondary" onClick={this.handleNext}>Next Page &rarr;</button>
+                </div>
             </div>
         )
     }
