01aaf4062a1f2086c51738389658a71ccc0424d1
ankitjangir01 <93923854+ankitjangir01@users.noreply.github.com>
2022-05-26 17:01:18 +0530

    implementing react router
    first installed react-router-dom module and then applied routes for different categories

--- src/News.js
+++ src/News.js
@@ -1,55 +1,67 @@
 import React, { Component } from 'react';
 import NewsItem from './NewsItem';
-
+import PropTypes from 'prop-types'
 
 export default class News extends Component {
     articles = [];
-
     constructor() {
         super();
         this.state = {
             articles: this.articles,
-            loading: false,
+            loading: true,
             page: 1
         }
     }
 
     async componentDidMount() {
-        let url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=48ccfd98f01b4ff995fe26caf9cbd7fb&pageSize=12';
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=48ccfd98f01b4ff995fe26caf9cbd7fb&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json();
-        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults })
+        this.setState({
+            articles: parsedData.articles,
+            totalResults: parsedData.totalResults,
+            loading: false,
+        })
 
     }
 
     handleNext = async () => {
-        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 12)) { }
-        else{
-            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=48ccfd98f01b4ff995fe26caf9cbd7fb&page=${this.state.page + 1}&pageSize=12`;
-            let data = await fetch(url);
-            let parsedData = await data.json();
-            this.setState({
-                articles: parsedData.articles,
-                page: this.state.page + 1,
-            })
-        }
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=48ccfd98f01b4ff995fe26caf9cbd7fb&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true, })
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+            articles: parsedData.articles,
+            page: this.state.page + 1,
+            loading: false,
+        })
     }
     handlePrev = async () => {
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=48ccfd98f01b4ff995fe26caf9cbd7fb&page=${this.state.page - 1}&pageSize=12`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=48ccfd98f01b4ff995fe26caf9cbd7fb&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true, });
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState({
             articles: parsedData.articles,
             page: this.state.page - 1,
+            loading: false,
         })
     }
 
     render() {
         return (
             <div className='container my-3'>
-                <h2>News Station - Top Headlines</h2>
-                <div className='row'>
-                    {this.state.articles.map((element) => {
+                <h1 className='text-center'>News Station - Top Headlines</h1>
+                {
+                    this.state.loading && <div className="d-flex justify-content-center">
+                        <div className="spinner-border" role="status">
+                            <span className="sr-only"></span>
+                        </div>
+                        <h4 className='mx-3'>Loading...</h4>
+                    </div>
+                }
+                <div className='row my-5'>
+                    {!this.state.loading && this.state.articles.map((element) => {
                         return (
                             <div className='col-md-4' key={element.url}>
                                 <NewsItem title={element.title} description={element.description ? element.description.slice(0, 100) : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
@@ -59,9 +71,19 @@ export default class News extends Component {
                 </div>
                 <div className='d-flex justify-content-around my-5'>
                     <button type="button" disabled={this.state.page <= 1} className="btn btn-secondary" onClick={this.handlePrev}>&larr; Previous Page</button>
-                    <button type="button" className="btn btn-secondary" onClick={this.handleNext}>Next Page &rarr;</button>
+                    <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} className="btn btn-secondary" onClick={this.handleNext}>Next Page &rarr;</button>
                 </div>
             </div>
         )
     }
+}
+
+News.defaultProps = {
+    country: 'in',
+    pageSize: 9,
+    category: 'general',
+}
+News.propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
 }
--- src/NewsItem.js
+++ src/NewsItem.js
@@ -6,7 +6,7 @@ export default class NewsItem extends Component {
         let { title, description, imageUrl, newsUrl } = this.props;
         return (
             <div>
-                <div className="card" style={{width: '18rem'}}>
+                <div className="card">
                     <img src={imageUrl ? imageUrl : 'https://images.indianexpress.com/2022/05/NASA-Mars-mission-plan-released-featured-May-2022.jpg'}
                         className="card-img-top" alt="..."/>
                         <div className="card-body">
