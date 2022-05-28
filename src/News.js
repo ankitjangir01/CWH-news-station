c90db58b361bd0e625b36dc7fac9f0826cd24b5d
ankitjangir01 <93923854+ankitjangir01@users.noreply.github.com>
2022-05-28 14:33:55 +0530

    implemented infinite scroll
    removed prev and next page buttons. Used infinite scroll instead using the npm package infinite-scroll-component.

--- src/News.js
+++ src/News.js
@@ -1,75 +1,81 @@
 import React, { Component } from 'react';
 import NewsItem from './NewsItem';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export default class News extends Component {
     articles = [];
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             articles: this.articles,
             loading: true,
-            page: 1
+            page: 1,
+            totalResults: 0,
         }
+        document.title = `${this.props.category} -News Station`;
     }
 
     async componentDidMount() {
         this.updatePage();
     }
 
-    async updatePage(){
+    async updatePage() {
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=48ccfd98f01b4ff995fe26caf9cbd7fb&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState({
-            articles: parsedData.articles,
+            page: this.state.page + 1,
+            articles: this.articles.concat(parsedData.articles),
             totalResults: parsedData.totalResults,
             loading: false,
         })
     }
 
-    handleNext = async () => {
+    fetchMoreData = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=48ccfd98f01b4ff995fe26caf9cbd7fb&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
         this.setState({
-            loading: true,
-            page: this.state.page + 1,
+            articles: this.state.articles.concat(parsedData.articles),
+            totalResults: parsedData.totalResults,
+            loading: false,
         })
-        this.updatePage();
-    }
-    handlePrev = async () => {
-        this.setState({
-            loading: true,
-            page: this.state.page - 1,
-        })
-        this.updatePage();
     }
 
     render() {
         return (
-            <div className='container my-3'>
-                <h1 className='text-center'>News Station - Top Headlines</h1>
-                {
-                    this.state.loading && <div className="d-flex justify-content-center">
-                        <div className="spinner-border" role="status">
-                            <span className="sr-only"></span>
-                        </div>
-                        <h4 className='mx-3'>Loading...</h4>
-                    </div>
-                }
-                <div className='row my-5'>
-                    {!this.state.loading && this.state.articles.map((element) => {
-                        return (
-                            <div className='col-md-4' key={element.url}>
-                                <NewsItem title={element.title} description={element.description ? element.description.slice(0, 100) : ""}
-                                imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} />
+            <>
+                <h1 className='text-center'>News Station -Top Headlines from {`${this.props.category}`}</h1>
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length != this.state.totalResults}
+                    loader={
+                        <>
+                            <div className='container text-center'>
+                                <div className="spinner-border" role="status">
+                                    <span className="sr-only"></span>
+                                </div>
+                                <h4>Loading...</h4>
                             </div>
-                        )
-                    })}
-                </div>
-                <div className='d-flex justify-content-around my-5'>
-                    <button type="button" disabled={this.state.page <= 1} className="btn btn-secondary" onClick={this.handlePrev}>&larr; Previous Page</button>
-                    <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} className="btn btn-secondary" onClick={this.handleNext}>Next Page &rarr;</button>
-                </div>
-            </div>
+                        </>
+                    }
+                >
+                    <div className='container'>
+                        <div className='row my-5'>
+                            {this.state.articles.map((element) => {
+                                return (
+                                    <div className='col-md-4' key={element.url}>
+                                        <NewsItem title={element.title} description={element.description ? element.description.slice(0, 100) : ""}
+                                            imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} />
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </InfiniteScroll>
+            </>
         )
     }
 }
--- src/NewsItem.js
+++ src/NewsItem.js
@@ -6,7 +6,7 @@ export default class NewsItem extends Component {
         let { title, description, imageUrl, newsUrl, author, date } = this.props;
         return (
             <div>
-                <div className="card">
+                <div className="card my-1">
                     <img src={imageUrl ? imageUrl : 'https://images.indianexpress.com/2022/05/NASA-Mars-mission-plan-released-featured-May-2022.jpg'}
                         className="card-img-top" alt="..."/>
                         <div className="card-body">
