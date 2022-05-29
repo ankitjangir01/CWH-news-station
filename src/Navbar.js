018cce25f47d80b987789ef7f28fbe6108a0e3e0
ankitjangir01 <93923854+ankitjangir01@users.noreply.github.com>
2022-05-29 22:41:35 +0530

    bug fixes

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,9 +6,12 @@ export default class Navbar extends Component {
     render() {
         return (
             <>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top navbar-collapse">
                     <a className="navbar-brand" href="/">News Station</a>
-                    
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item"><Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link></li>
@@ -19,7 +22,7 @@ export default class Navbar extends Component {
                             <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
                             <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
                             <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
-                            </ul>
+                        </ul>
                     </div>
                 </nav>
             </>
